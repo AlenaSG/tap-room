642c1e34167e0513acab25734de016c7298b3127
Alena Golovina and Anna Timofeeva <alena+anna@>
2017-08-16 15:13:09 -0700

    add property and event binding

--- app/app.component.ts
+++ app/app.component.ts
@@ -7,7 +7,9 @@ import { Component } from '@angular/core';
     <h1>Tap Room</h1>
     <h3>{{currentSelection}}</h3>
     <ul>
-       <li>{{firstKeg.name}}</li>
+       <li (click)="isEmpty(currentKeg)" *ngFor="let currentKeg of kegs">{{currentKeg.name}}
+       <button (click)="editTask()">Edit!</button>
+       </li>
      </ul>
   </div>
   `
@@ -15,7 +17,31 @@ import { Component } from '@angular/core';
 
 export class AppComponent {
   currentSelection: string = 'Summer Selection';
-  firstKeg = {
-    name: "Budwiser"
+  kegs: Keg[] = [
+    new Keg('LA TRAPPE QUADRUPEL'),
+    new Keg('MURPHY’S IRISH'),
+    new Keg('BLUE MOON BELGIAN')
+  ];
+
+  editTask() {
+      alert("You just requested to edit a Keg!");
+    }
+
+
+    isEmpty(clickedKeg: Keg) {
+      if(clickedKeg.empty === true) {
+        alert("This keg is empty! Please replace.");
+      } else {
+        alert("This keg is full!");
+      }
+    }
+
   }
+
+
+}
+
+export class Keg {
+  public empty: boolean = false;
+  constructor(public name: string) { }
 }
