1c63dda62001b9dbc776bcfec7fca32e1a0c627a
Alena Golovina and Anna Timofeeyva <alena+anna@>
2017-08-17 15:05:58 -0700

    add filtering with pipe

--- app/keg-list.component.ts
+++ app/keg-list.component.ts
@@ -4,17 +4,31 @@ import { Keg } from './keg.model';
 @Component({
   selector: 'keg-list',
   template: `
+  <select (change)="onChange($event.target.value)">
+     <option value="allKegs">All Kegs</option>
+     <option value="emptyKegs">Empty Kegs</option>
+     <option value="fullKegs" selected="selected">Full Kegs</option>
+   </select>
+
+
+
   <ul class="list-group">
-     <li class="list-group-item" (click)="isEmpty(currentKeg)" *ngFor="let currentKeg of childKegList"><span [class]="percentageColor(currentKeg)">{{currentKeg.name}}</span><span class="badge">{{currentKeg.percentage}}</span>
+     <li class="list-group-item" (click)="isEmpty(currentKeg)" *ngFor="let currentKeg of childKegList | completeness:filterByCompleteness"><span [class]="percentageColor(currentKeg)">{{currentKeg.name}}</span><span class="badge">{{currentKeg.percentage}}</span>
+
+     <input *ngIf="currentKeg.empty === true" type="checkbox" checked (click)="toggleEmpty(currentKeg, false)"/>
+      <input *ngIf="currentKeg.empty === false" type="checkbox" (click)="toggleEmpty(currentKeg, true)"/>
+
     <button class="pull-right" (click)="editButtonHasBeenClicked(currentKeg)">Edit!</button>
      </li>
    </ul>
+
   `
 })
 
 export class KegListComponent {
   @Input() childKegList: Keg[];
   @Output() clickSender = new EventEmitter();
+  filterByCompleteness: string = "fullKegs";
 
       isEmpty(clickedKeg: Keg) {
         if(clickedKeg.empty === true) {
@@ -33,6 +47,14 @@ export class KegListComponent {
         return "bg-info";
       }
     }
+
+    onChange(optionFromMenu) {
+      this.filterByCompleteness = optionFromMenu;
+    }
+
+    toggleEmpty(clickedKeg: Keg, setCompleteness: boolean) {
+         clickedKeg.empty = setCompleteness;
+       }
 
     editButtonHasBeenClicked(kegToEdit: Keg) {
     this.clickSender.emit(kegToEdit);
